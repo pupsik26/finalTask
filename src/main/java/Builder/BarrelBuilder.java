bc8c4b2b9cb8a4560ff3efa88d0b8acb1e0ab2a3
Dany <ZakharovaDarya2021@yandex.ru>
2026-07-06 20:01:51 +0300

    Модели классов + Builder

---
+++ src/main/java/Builder/BarrelBuilder.java
@@ -0,0 +1,25 @@
+package Builder;
+
+import ModelClass.Barrel;
+
+public class BarrelBuilder extends Barrel {
+
+    public BarrelBuilder() {
+        super();
+    }
+
+    public BarrelBuilder setVolume(double volume) {
+        this.volume = volume;
+        return this;
+    }
+
+    public BarrelBuilder setStoredMaterial(String storedMaterial) {
+        this.storedMaterial = storedMaterial;
+        return this;
+    }
+
+    public BarrelBuilder setMaterial(String material) {
+        this.material = material;
+        return this;
+    }
+}
---
+++ src/main/java/Builder/UserBuilder.java
@@ -0,0 +1,25 @@
+package Builder;
+
+import ModelClass.User;
+
+public class UserBuilder extends User {
+
+    public UserBuilder() {
+        super();
+    }
+
+    public UserBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
