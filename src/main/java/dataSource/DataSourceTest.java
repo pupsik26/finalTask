f5b5dc72075d9e4732c7c101701cad54a5f59aab
pyatinva <viktormgn74@gmail.com>
2026-07-15 19:03:47 +0500

    Updated tests for FileReader and FileWriter

--- src/main/java/dataSource/DataSourceTest.java
+++ src/main/java/dataSource/DataSourceTest.java
@@ -9,89 +9,108 @@ import java.util.List;
 public class DataSourceTest {
     public static void main(String[] args) {
         int errors = 0;
-        Generator generator = new Generator();
-        List<Barrel> a = generator.readBarrels(2);
-        List<Bus> b = generator.readBus(3);
-        List<Car> c = generator.readCars(4);
-        List<Student> s = generator.readStudents(5);
-        List<User> u = generator.readUsers(6);
-
-        if (a.size() != 2) {
-            errors++;
-            System.out.println("Barrel error");
-        }
-        if (a.size() != 3) {
-            errors++;
-            System.out.println("Bus error");
-        }
-        if (a.size() != 4) {
-            errors++;
-            System.out.println("Car error");
-        }
-        if (a.size() != 5) {
-            errors++;
-            System.out.println("Student error");
-        }
-        if (a.size() != 6) {
-            errors++;
-            System.out.println("User error");
-        }
-
-        try {
-            a = FileReader.readBarrel("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\barrel.csv");
-            b = FileReader.readBus("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\bus.csv");
-            c = FileReader.readCar("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\car.csv");
-            s = FileReader.readStudent("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\student.csv");
-            u = FileReader.readUser("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\user.csv");
-
-            System.out.println(a.size());
-            System.out.println(b.size());
-            System.out.println(c.size());
-            System.out.println(s.size());
-            System.out.println(u.size());
-        } catch (Exception e) {
-            errors++;
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            a = FileReader.readBarrel("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\barrel.json");
-            b = FileReader.readBus("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\bus.json");
-            c = FileReader.readCar("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\car.json");
-            s = FileReader.readStudent("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\student.json");
-            u = FileReader.readUser("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\user.json");
-
-            System.out.println(a.size());
-            System.out.println(b.size());
-            System.out.println(c.size());
-            System.out.println(s.size());
-            System.out.println(u.size());
-        } catch (Exception e) {
-            errors++;
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            a = FileReader.readBarrel("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\barrel.xml");
-            b = FileReader.readBus("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\bus.xml");
-            c = FileReader.readCar("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\car.xml");
-            s = FileReader.readStudent("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\student.xml");
-            u = FileReader.readUser("F:\\JavaProjects\\FinalTask\\src\\main\\java\\dataSource\\fileReader\\examples\\user.xml");
-
-            System.out.println(a.size());
-            System.out.println(b.size());
-            System.out.println(c.size());
-            System.out.println(s.size());
-            System.out.println(u.size());
-        } catch (Exception e) {
-            errors++;
-            System.out.println(e.getMessage());
-        }
+        errors += testGenerator();
+        errors += testCsv();
+        errors += testJson();
+        errors += testXml();
 
         if (errors > 0) {
             System.out.printf("Test ended with %d errors!", errors);
         } else {
             System.out.print("Test ended successfully!");
         }
+    }
+
+    private static int testGenerator() {
+        try {
+            System.out.println("Generator:");
+            Generator generator = new Generator();
+            List<Barrel> a = generator.readBarrels(2);
+            List<Bus> b = generator.readBus(3);
+            List<Car> c = generator.readCars(4);
+            List<Student> s = generator.readStudents(5);
+            List<User> u = generator.readUsers(6);
+
+            writeArray("Barrel", a);
+            writeArray("Bus", b);
+            writeArray("Car", c);
+            writeArray("Student", s);
+            writeArray("User", u);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int testCsv() {
+        try {
+            System.out.println("Read from CSV:");
+            List<Barrel> a = FileReader.readBarrel("src\\main\\java\\dataSource\\fileReader\\examples\\barrel.csv");
+            List<Bus> b = FileReader.readBus("src\\main\\java\\dataSource\\fileReader\\examples\\bus.csv");
+            List<Car> c = FileReader.readCar("src\\main\\java\\dataSource\\fileReader\\examples\\car.csv");
+            List<Student> s = FileReader.readStudent("src\\main\\java\\dataSource\\fileReader\\examples\\student.csv");
+//            List<User> u = FileReader.readUser("src\\main\\java\\dataSource\\fileReader\\examples\\user.csv");
+
+            writeArray("Barrel", a);
+            writeArray("Bus", b);
+            writeArray("Car", c);
+            writeArray("Student", s);
+//            writeArray("User", u);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int testJson() {
+
+        try {
+            System.out.println("Read from JSON:");
+            List<Barrel> a = FileReader.readBarrel("src\\main\\java\\dataSource\\fileReader\\examples\\barrel.json");
+            List<Bus> b = FileReader.readBus("src\\main\\java\\dataSource\\fileReader\\examples\\bus.json");
+            List<Car> c = FileReader.readCar("src\\main\\java\\dataSource\\fileReader\\examples\\car.json");
+            List<Student> s = FileReader.readStudent("src\\main\\java\\dataSource\\fileReader\\examples\\student.json");
+//            List<User> u = FileReader.readUser("src\\main\\java\\dataSource\\fileReader\\examples\\user.json");
+            List<User> u = FileReader.readUser("src\\main\\java\\dataSource\\fileReader\\examples\\user_valid.json");
+
+            writeArray("Barrel", a);
+            writeArray("Bus", b);
+            writeArray("Car", c);
+            writeArray("Student", s);
+            writeArray("User", u);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int testXml() {
+        try {
+            System.out.println("Read from XML:");
+            List<Barrel> a = FileReader.readBarrel("src\\main\\java\\dataSource\\fileReader\\examples\\barrel.xml");
+            List<Bus> b = FileReader.readBus("src\\main\\java\\dataSource\\fileReader\\examples\\bus.xml");
+            List<Car> c = FileReader.readCar("src\\main\\java\\dataSource\\fileReader\\examples\\car.xml");
+            List<Student> s = FileReader.readStudent("src\\main\\java\\dataSource\\fileReader\\examples\\student.xml");
+            List<User> u = FileReader.readUser("src\\main\\java\\dataSource\\fileReader\\examples\\user.xml");
+
+            writeArray("Barrel", a);
+            writeArray("Bus", b);
+            writeArray("Car", c);
+            writeArray("Student", s);
+            writeArray("User", u);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    private static <T> void writeArray(String className, List<T> array) {
+        System.out.println(className + " " + array.size());
+        array.forEach(System.out::println);
+        System.out.println();
     }
 }
--- src/main/java/fileWriter/FileWriterTest.java
+++ src/main/java/fileWriter/FileWriterTest.java
@@ -2,6 +2,7 @@ package fileWriter;
 
 
 import daryaClassStream.ModelBuilderClass.Barrel;
+import dataSource.fileReader.FileReader;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 public class FileWriterTest {
     public static void main(String[] args) {
         testBarrels();
+        testReadFromWritten();
     }
 
     private static void testBarrels() {
@@ -32,6 +34,22 @@ public class FileWriterTest {
             writer.writeXml("src\\main\\java\\fileWriter\\test\\barrel.xml", b);
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void testReadFromWritten() {
+        try {
+            List<Barrel> a = FileReader.readBarrel("src\\main\\java\\fileWriter\\test\\barrel.csv");
+            a.forEach(System.out::println);
+            System.out.println();
+            a = FileReader.readBarrel("src\\main\\java\\fileWriter\\test\\barrel.json");
+            a.forEach(System.out::println);
+            System.out.println();
+            a = FileReader.readBarrel("src\\main\\java\\fileWriter\\test\\barrel.xml");
+            a.forEach(System.out::println);
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
